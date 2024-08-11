b183ba7246ffd4d1d6c136e0912c40d17630f3b2
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-08-12 00:37:40 +0300

    Update MainActivity.kt

--- JCompose/app/src/main/java/com/example/jcompose/MainActivity.kt
+++ JCompose/app/src/main/java/com/example/jcompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jcompose.ui.theme.JComposeTheme
@@ -23,25 +24,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JComposeTheme() {
+                Greeting(name = "Anderea")
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column {
 
-
-
-                    }
-                }
             }
         }
     }
 }
 
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+        color = Color.Red)
 }
 
 
