ebdf299e22fd00f4bf43e3eb632375d60d56efe9
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-10-01 18:02:18 +0300

    Update TwoActivity.kt

--- JCompose/app/src/main/java/com/example/jcompose/TwoActivity.kt
+++ JCompose/app/src/main/java/com/example/jcompose/TwoActivity.kt
@@ -42,7 +42,10 @@ import androidx.compose.ui.unit.dp
 import com.example.jcompose.ui.theme.JComposeTheme
 import androidx.compose.runtime.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
