8efbad36c4cec9ea4d46d98910b3b6a701e9287a
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-10-02 12:50:00 +0300

    Update Task.kt

--- JCompose/app/src/main/java/com/example/jcompose/Task.kt
+++ JCompose/app/src/main/java/com/example/jcompose/Task.kt
@@ -1,4 +1,17 @@
 package com.example.jcompose
 
-class Task {
+// Task.kt (Model)
+data class Task(val id: Int, val name: String)
+
+// TaskRepository.kt
+class TaskRepository {
+    private val tasks = mutableListOf<Task>()
+
+    fun getTasks(): List<Task> {
+        return tasks
+    }
+
+    fun addTask(task: Task) {
+        tasks.add(task)
+    }
 }
