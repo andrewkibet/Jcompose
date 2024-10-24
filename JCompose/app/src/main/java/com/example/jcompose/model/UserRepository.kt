cb7f3ddabf6ca1d1d2454c90ba00e643addfa5c6
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-10-24 13:10:46 +0300

    Update UserRepository.kt

--- JCompose/app/src/main/java/com/example/jcompose/model/UserRepository.kt
+++ JCompose/app/src/main/java/com/example/jcompose/model/UserRepository.kt
@@ -1,7 +1,12 @@
 package com.example.jcompose.network.com.example.jcompose.model
 
+import android.service.autofill.UserData
+import kotlinx.coroutines.delay
+
 class UserRepository {
-    fun fetchuserData():{
+    suspend fun fetchuserData(): com.example.jcompose.network.com.example.jcompose.model.UserData {
+        delay(2000)
+        return UserData("SS",25)
 
     }
-}
+    }
