a53f4962b025ec06d94668235231dc338d0ddf99
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-10-24 15:21:02 +0300

    Update MVVMActivity.kt

--- JCompose/app/src/main/java/com/example/jcompose/MVVMActivity.kt
+++ JCompose/app/src/main/java/com/example/jcompose/MVVMActivity.kt
@@ -17,15 +17,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.jcompose.network.com.example.jcompose.view.HomePage
+import com.example.jcompose.network.com.example.jcompose.viewmodel.HomeViewModel
 import com.example.jcompose.ui.theme.JComposeTheme
 
 class MVVMActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class]
+
         setContent {
             JComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomePage(modifier = Modifier.padding(innerPadding),homeViewModel)
 
                 }
             }
@@ -33,49 +40,12 @@ class MVVMActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-/*
-@Composable
-fun TaskApp(viewModel: TaskViewModel) {
-    var newTaskName by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        TextField(
-            value = newTaskName,
-            onValueChange = { newTaskName = it },
-            label = { Text("New Task") }
-        )
 
-        Button(onClick = {
-            if (newTaskName.isNotBlank()) {
-                viewModel.addTask(newTaskName)
-                newTaskName = ""
-            }
-        }) {
-            Text("Add Task")
-        }
 
-        LazyColumn {
-            items(viewModel.tasks.value) { task ->
-                Text(text = task.name)
-            }
-        }
-    }
-}
-
- */
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
-    JComposeTheme {
-        Greeting("Android")
-    }
-}
+   }
