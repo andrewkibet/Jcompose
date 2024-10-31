d3ac2c3c1bd58dfe9735c86b6d09fcbb24eb55e7
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-10-31 11:47:28 +0300

    Update HomeViewModel.kt

--- JCompose/app/src/main/java/com/example/jcompose/viewmodel/HomeViewModel.kt
+++ JCompose/app/src/main/java/com/example/jcompose/viewmodel/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.jcompose.network.com.example.jcompose.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.LiveData
@@ -16,8 +17,15 @@ class HomeViewModel : ViewModel() {
 
     fun getUserData() {
         viewModelScope.launch {
-            val userResult = userRepository.fetchUserData()
-            _userData.postValue(userResult)
+            try {
+                val userResult = userRepository.fetchUserData()
+                _userData.postValue(userResult)
+            }
+            catch (e : Exception){
+                Log.e("HomeViewmodel", "Error fetching user data",e)
+
+            }
+
         }
     }
 }
