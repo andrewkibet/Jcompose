6dba109a94025fbff2fbf449cac51748f7d4ed58
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-11-06 01:12:06 +0300

    Create MVVMActivity.kt

---
+++ app/src/main/java/com/example/jcompose/MVVMActivity.kt
@@ -0,0 +1,51 @@
+package com.example.jcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.jcompose.network.com.example.jcompose.view.HomePage
+import com.example.jcompose.network.com.example.jcompose.viewmodel.HomeViewModel
+import com.example.jcompose.ui.theme.JComposeTheme
+
+class MVVMActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class]
+
+        setContent {
+            JComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomePage(modifier = Modifier.padding(innerPadding),homeViewModel)
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+   }
