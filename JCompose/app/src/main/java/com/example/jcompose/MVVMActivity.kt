ae828c2e205bab37ac6d0b4ee2ded663ed9067f2
Kibet <61360677+andrewkibet@users.noreply.github.com>
2025-01-29 13:25:29 +0300

    Update MVVMActivity.kt

--- JCompose/app/src/main/java/com/example/jcompose/MVVMActivity.kt
+++ JCompose/app/src/main/java/com/example/jcompose/MVVMActivity.kt
@@ -3,49 +3,41 @@ package com.example.jcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.jcompose.network.com.example.jcompose.view.HomePage
-import com.example.jcompose.network.com.example.jcompose.viewmodel.HomeViewModel
+import com.example.jcompose.view.HomePage
+import com.example.jcompose.viewmodel.HomeViewModel
 import com.example.jcompose.ui.theme.JComposeTheme
 
 class MVVMActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class]
+        // Correct ViewModel instantiation
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         setContent {
             JComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomePage(modifier = Modifier.padding(innerPadding),homeViewModel)
-
+                    HomePage(
+                        modifier = Modifier.padding(innerPadding),
+                        viewModel = homeViewModel
+                    )
                 }
             }
         }
     }
 }
 
-
-
-
-
-
+// Preview function for UI testing
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
-   }
+fun PreviewHomePage() {
+    val homeViewModel = HomeViewModel()
+    HomePage(viewModel = homeViewModel)
+}
